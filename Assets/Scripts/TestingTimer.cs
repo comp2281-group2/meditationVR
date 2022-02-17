aca11ab7ef94f704cee90a6356caba15031fd1f7
Dmitrie <dmitriebivol@gmail.com>
2022-02-17 14:35:00 +0000

    Implemented parsiong of subtitles from text file. Timed the rest of the lines. Update all the scenes to work with this

--- Assets/Scripts/TestingTimer.cs
+++ Assets/Scripts/TestingTimer.cs
@@ -3,45 +3,51 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using System;
+using System.IO;
 using TMPro;
 
 public class TestingTimer : MonoBehaviour
 {
-    private bool isActive = false;
+    
+    public TextAsset subtitleFile;     // format of each line:  <subtitle text>#<duration of the line, time when next line begins-time when this line starts>     # is the delimeter; Drag the text file in the inspector
     public TMP_Text textComponent;
     private bool isEnd = false;
+    private bool isActive = false;
+    private List<SubtitleLine> lines = new List<SubtitleLine>();
 
-    private class SubtitleLine
+    class SubtitleLine
     {
         public string subtitle { get; set; }
         public int timeDelay { get; set; }
     }
-
-    private List<SubtitleLine> lines = new List<SubtitleLine> {
-        new SubtitleLine { subtitle="Find a relaxed, comfortable position", timeDelay= 3},
-        new SubtitleLine { subtitle="Seated on a chair or on the floor, on a cushion",timeDelay=10},
-        new SubtitleLine { subtitle="Keep your back upright, but not too tight",timeDelay=6},
-        new SubtitleLine { subtitle="Hands resting wherever they're comfortable",timeDelay=6},
-        new SubtitleLine { subtitle="Tongue on the roof of your mouth or wherever it's comfortable.",timeDelay=7},
-        new SubtitleLine { subtitle="And you can notice your body",timeDelay=3},
-        new SubtitleLine { subtitle="From the inside",timeDelay=3},
-        new SubtitleLine { subtitle="Noticing the shape of your body, the weight, touch",timeDelay=11},
-        new SubtitleLine { subtitle="And let yourself relax",timeDelay=3},
-        new SubtitleLine { subtitle="And become curious about your body",timeDelay=5},
-        new SubtitleLine { subtitle="Seated here",timeDelay=2},
-        new SubtitleLine { subtitle="The sensations of your body, the touch",timeDelay=6},
-        new SubtitleLine { subtitle="The connection with the floor, the chair",timeDelay=5},
-        new SubtitleLine { subtitle="Relax any areas of tightness or tension",timeDelay=8},
-        new SubtitleLine { subtitle="Just breathe, soften",timeDelay=7},
-        new SubtitleLine { subtitle="And now begin to tune into your breath",timeDelay=6}
-    };
-    private int lineNumber = 0;
-    /*public void Start()
+   
+    void Start()
     {
-        StartCoroutine(Timer(0));
+        string text = subtitleFile.text;  //this is the content as string
+        using (StringReader reader = new StringReader(text))
+        {
+            string line = string.Empty;
+            do
+            {
+                line = reader.ReadLine();
+                if (line != null)
+                {
+                    string[] splitLine = line.Split("#");
+                    if (splitLine.Length == 2) 
+                    {
+                        int duration = int.Parse(splitLine[1]);
+                        string subtitle = splitLine[0];
+                        SubtitleLine subtitleLineObject = new SubtitleLine { subtitle = subtitle, timeDelay = duration };
+                        lines.Add(subtitleLineObject);
+                        
+                    }
+                }
 
-    }*/
+            } while (line != null);
+        }
+    }
 
+    private int lineNumber = 0;
     public void Update() 
     {
         if (isActive == false && isEnd == false) 
@@ -63,7 +69,6 @@ public class TestingTimer : MonoBehaviour
         isActive = true;
         yield return new WaitForSeconds(duration);
         isActive = false;
-        Debug.Log("Timer has ended");
     }
 
     private void ChangeTextTo(string text)
