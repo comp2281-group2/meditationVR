209f127aac4a544387bd5b86d8d9676c9027b7fe
JoshuaJackNewbould <JoshuaNewbould@outlook.com>
2022-03-02 20:28:28 +0000

    menu sliders and camera height

---
+++ Assets/EnviromentVolume.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class EnviromentVolume : MonoBehaviour {
+    public AudioMixer audioMixer;
+    public void VolSetter(float vol) {
+        audioMixer.SetFloat("EnviromentVolume", vol);
+    }
+}
+
---
+++ Assets/InstructionVolume.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class InstructionVolume : MonoBehaviour {
+    public AudioMixer audioMixer;
+    public void VolSetter(float vol) {
+        audioMixer.SetFloat("InstructionVolume", vol);
+    }
+}
+
