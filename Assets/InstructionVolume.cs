e3aae603968184bef2ca8c104317e25fff737f83
Tom Abbott <tommcmin@gmail.com>
2022-03-07 23:02:51 +0000

    Rig audio sliders

--- Assets/InstructionVolume.cs
+++ Assets/InstructionVolume.cs
@@ -1,12 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class InstructionVolume : MonoBehaviour {
+    private const string GROUP_NAME = "Instructions";
+
     public AudioMixer audioMixer;
+    private AudioVolumes vols;
+
+    public void Awake() {
+        // load the saved volume from SavedPreferences
+        vols = AudioVolumes.FromPreferences();
+        audioMixer.SetFloat(GROUP_NAME, Mathf.Log10(vols.Instructions) * 20);
+    }
+
     public void VolSetter(float vol) {
-        audioMixer.SetFloat("InstructionVolume", Mathf.Log10(vol) * 20);
+        audioMixer.SetFloat(GROUP_NAME, Mathf.Log10(vol) * 20);
     }
 }
-
--- Assets/MasterVolume.cs
+++ Assets/MasterVolume.cs
@@ -1,11 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class MasterVolume : MonoBehaviour{
+    private const string GROUP_NAME = "Master";
+
     public AudioMixer audioMixer;
+    private AudioVolumes vols;
+
+    public void Awake() {
+        // load the saved volume from SavedPreferences
+        vols = AudioVolumes.FromPreferences();
+        audioMixer.SetFloat(GROUP_NAME, Mathf.Log10(vols.Master) * 20);
+    }
+
     public void VolSetter(float vol) {
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(vol) * 20);
+        audioMixer.SetFloat(GROUP_NAME, Mathf.Log10(vol) * 20);
     }
 }
