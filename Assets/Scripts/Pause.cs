3249d6af656cc8e6b713b1d92767d2449804a1b8
Dmitrie <dmitriebivol@gmail.com>
2022-03-11 22:36:42 +0000

    in game menu canvas. No scripting

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,23 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    void Start() {
+        PauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetUp(Button.Start))
+        {
+            Debug.Log("escape was pressed");
+            PauseMenu.SetActive(true);
+        }
+    }
+}
+*/
