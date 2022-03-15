7b7b84f04b836806927519211c68b68e49c9d838
Dmitrie <dmitriebivol@gmail.com>
2022-03-15 16:22:36 +0000

    Pause menu initial. Can't seem to e able to bind input to methods.

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,23 +1,67 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Pause : MonoBehaviour
 {
     public GameObject PauseMenu;
+    public bool isPaused;
+    public bool isPauseMenuDisplayed;
+    public AudioSource mainCameraComponent;
 
-    void Start() {
-        PauseMenu.SetActive(false);
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetUp(Button.Start))
+        if (isPaused)
         {
-            Debug.Log("escape was pressed");
-            PauseMenu.SetActive(true);
+            if (!isPauseMenuDisplayed) {
+                PauseGame();
+            }
+        }
+        else
+        {
+            if (isPauseMenuDisplayed)
+            {
+                ResumeGame();
+            }
+        }
+        
+        
+    }
+
+    public void ResumeGame() {
+        isPaused = false;
+        isPauseMenuDisplayed = false;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        //resume adio
+        mainCameraComponent.Play();
+
+    }
+    public void PauseGame() {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        //pause audio
+        mainCameraComponent.Pause();
+    }
+    private void OnMenu(InputValue buttonValue) {
+        if (buttonValue.isPressed) {
+            isPaused = !isPaused;
         }
     }
+    private void OnAny(InputValue value) {
+        Debug.Log("Esc pressed");
+    }
+    void OnPause(InputValue value)
+    {
+        Debug.Log("Esc pressed");
+    }
+    
 }
-*/
